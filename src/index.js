441c0a8ae57f679fcce188e62f6d271fd8f49c1c
clroot <abcdkh1209@gmail.com>
2022-01-12 00:12:05 +0900

    remove get-port package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import getPort from 'get-port';
 import app from './main';
 import sequelize from './database';
 import { syncAllModel } from './models';
@@ -8,19 +7,18 @@ dotenv.config();
 
 export const startServer = async (port = process.env.PORT || 4000, callback = undefined) => {
   if (process.env.NODE_ENV === 'test') {
-    port = await getPort({ port: getPort.makeRange(4001, 5000) });
+    port = 0;
   }
 
   await sequelize.authenticate();
   await syncAllModel();
 
-  console.log(`Server is starting on port ${port}`);
   const server = await app.listen(port);
 
   return callback ? callback(server) : Promise.resolve(server);
 };
 
 
-startServer().then(() => {
-  console.log('Server is started successfully');
+startServer().then((server) => {
+  console.log(`Server is started on port ${server.address().port}`);
 });
