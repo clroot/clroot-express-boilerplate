9b440323b859eb12853f52c0c6197c348d7f3951
clroot <abcdkh1209@gmail.com>
2022-01-13 11:23:25 +0900

    모델 synchronization 설정 변경

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,5 @@ import UserModel from './user';
 export const User = UserModel;
 
 export const syncAllModel = async () => {
-  await User.sync({ force: true });
+  await User.sync({ alter: true });
 };
