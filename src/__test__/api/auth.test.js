4868a31c00a68a0f1983495ceb05e3cf6cdfc5dc
clroot <abcdkh1209@gmail.com>
2022-01-13 15:32:36 +0900

    implement authApi - register

---
+++ src/__test__/api/auth.test.js
@@ -0,0 +1,71 @@
+import request from 'supertest';
+import httpStatus from 'http-status';
+import { closeServer, startServer } from '../../index';
+import { User } from '../../models';
+import { UserService } from '../../service';
+
+const testUserEmail = 'test@email.com';
+const testUsername = 'test';
+const testUserPassword = 'test-password';
+
+const payload = {
+  email: testUserEmail,
+  username: testUsername,
+  password: testUserPassword,
+};
+
+const removeTestUser = async () => {
+  await User.destroy({
+    where: {
+      email: testUserEmail,
+    },
+  });
+};
+
+describe('auth API 의', () => {
+  const apiPrefix = '/api/v1/auth';
+  let server;
+
+  beforeAll(async () => {
+    server = await startServer();
+  });
+
+  afterAll(async () => {
+    await closeServer(server);
+  });
+
+  describe('/register 는', () => {
+    const route = `${apiPrefix}/register`;
+
+    describe('성공시', () => {
+      afterAll(async () => {
+        await removeTestUser();
+      });
+
+      it('user 객체를 return 한다.', async () => {
+        const { body } = await request(server)
+          .post(route)
+          .send(payload)
+          .expect(httpStatus.CREATED);
+
+        expect(body.email).toBe(testUserEmail);
+        expect(body.username).toBe(testUsername);
+      });
+    });
+
+    describe('중복된 이메일이 존재하면', () => {
+      beforeAll(async () => {
+        await UserService.register(payload);
+      });
+
+      it('CONFLICT 를 return 한다.', async () => {
+        const { body } = await request(server)
+          .post(route)
+          .send(payload)
+          .expect(httpStatus.CONFLICT);
+
+        console.log(body);
+      });
+    });
+  });
+});
--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,9 +1,31 @@
 import { Router } from 'express';
+import httpStatus from 'http-status';
+import { UserService } from '../../service';
+import { UserDuplicateError } from '../../error';
 
 const authApi = Router();
 
-authApi.post('/register', (req, res, next) => {
-  next();
+authApi.post('/register', async (req, res) => {
+  const { email, username, password } = req.body;
+
+  try {
+    await UserService.register({ email, username, password });
+    const createdUser = await UserService.findByEmail(email);
+
+    res.status(httpStatus.CREATED);
+
+    return res.json(createdUser);
+  } catch (err) {
+    if (err instanceof UserDuplicateError) {
+      res.status(httpStatus.CONFLICT);
+
+      return res.json({
+        'error': err.message,
+      });
+    } else {
+      throw err;
+    }
+  }
 });
 authApi.post('/login', (req, res, next) => {
   next();
